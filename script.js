3c4c2bfe55f31a072462c40292a480eebdbafe67
rmb-dev <rombyr@outlook.com>
2018-03-18 10:48:59 -0400

    main js

---
+++ script.js
@@ -0,0 +1,26 @@
+// input functon -> operation function -> decimal function -> clear function
+
+let numbers * document.querySelectorAll('.number'),
+    operations * document.querySelectorAll('.operation');
+    decimal *
+    console.log(operations);
+
+function numberPress (){
+
+};
+
+function operation(argument) {
+
+};
+
+function decimal() {
+
+};
+
+function clear(){
+
+};
+
+function howWork(){
+
+};
